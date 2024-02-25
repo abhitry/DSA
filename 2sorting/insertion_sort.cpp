7fe449437776f50a223f1260591abde73d2ad961
Abhishek Masne <abhishekmasne2015@gmail.com>
2024-02-25 16:41:17 +0530

    INSERTION SORT

--- 2sorting/insertion_sort.cpp
+++ 2sorting/insertion_sort.cpp
@@ -1,25 +1,42 @@
 #include<iostream>
 using namespace std;
+void rec_insertion(int *nums,int  i,int n)
+{
+    if(i==n)
+    {
+        return;
+    }
+    int j=i-1;
+    int temp=nums[i];
+    while(j>=0 && nums[j]>temp)
+    {
+        nums[j+1]=nums[j];
+        j--;
+    }
+    nums[j+1]=temp;
+    rec_insertion(nums,i+1,n);
+}
 int main()
 {
     int nums[10]={9,2,7,4,5,8,1,0,6,3};
     int n=10;
     int j;
-    for(int i=0;i<n;i++)
-    {
-        int temp=nums[i];
-        for( j=i-1;j>=0;j--)
-        {
-            if(nums[j]>temp)
-            {
-                nums[j+1]=nums[j];
-            }
-            else{
-                break;
-            }
-        }
-        nums[j+1]=temp;
-    }
+    // for(int i=0;i<n;i++)
+    // {
+    //     int temp=nums[i];
+    //     for( j=i-1;j>=0;j--)
+    //     {
+    //         if(nums[j]>temp)
+    //         {
+    //             nums[j+1]=nums[j];
+    //         }
+    //         else{
+    //             break;
+    //         }
+    //     }
+    //     nums[j+1]=temp;
+    // }
+    rec_insertion(nums,0,n);
     for(int i=0;i<n;i++)
     {
         cout<<nums[i]<<" ";
