9c6fb8648a7aace3532462ec2a3b4e80e0aa955f
Abhishek Masne <abhishekmasne2015@gmail.com>
2024-02-25 17:36:51 +0530

    SORT

---
+++ 2sorting/merge_sort.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+void merge(int *nums,int low,int mid,int up)
+{
+    int i=low;
+    int j=mid+1;
+    int* temp=new int[up-low+1];
+    int k=0;
+    while(i<=mid && j<=up)
+    {
+        if(nums[i]<nums[j])
+        {
+            temp[k]=nums[i];
+            k++;i++;
+        }
+        else
+        {
+            temp[k]=nums[j];
+            k++;
+            j++;
+        }   
+    }
+    while(i<=mid)
+    {
+        temp[k]=nums[i];
+        k++;i++;
+    }
+    while(j<=up)
+    {
+        temp[k]=nums[j];
+        k++;j++;
+    }
+    for(int i=low;i<=up;i++)
+    {
+        nums[i]=temp[i-low];
+    }
+}
+void mergesort(int *nums,int low,int up)
+{
+    if(low<up)
+    {
+        int mid=(low+up)/2;
+        mergesort(nums,low,mid);
+        mergesort(nums,mid+1,up);
+        merge(nums,low,mid,up);
+    }
+}
+int main()
+{
+    int nums[10]={9,2,7,4,5,8,1,0,6,3};
+    int n=10;
+    int j;
+    mergesort(nums,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+}
---
+++ 2sorting/quick_sort.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+int partition(int *nums,int low,int up)
+{
+    int pivot=nums[low];
+    int p=low+1;
+    int q=up;
+    while (1)
+    {
+        while(p<=up && nums[p]<pivot)
+        {
+            p++;
+        }
+        while(nums[q]>pivot)
+        {
+            q--;
+        }
+        if(p<q)
+        {
+            swap(nums[p],nums[q]);
+        }
+        else{
+            break;
+        }
+    }
+    swap(nums[q],nums[low]);
+    return q;
+    
+}
+void mergesort(int *nums,int low,int up)
+{
+    if(low<up)
+    {
+        int k=partition(nums,low,up);
+        mergesort(nums,low,k-1);
+        mergesort(nums,k+1,up);
+    }
+}
+int main()
+{
+    int nums[10]={9,2,7,4,5,8,1,0,6,3};
+    int n=10;
+    int j;
+    mergesort(nums,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<nums[i]<<" ";
+    }
+}
