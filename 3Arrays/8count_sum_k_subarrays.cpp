c0c7c6b26cd2d7cba2e69e2a128585e5599a7c06
Abhishek Masne <abhishekmasne2015@gmail.com>
2024-02-27 01:32:23 +0530

    COUNT SUM K SUBARRAYS

---
+++ 3Arrays/8count_sum_k_subarrays.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<map>
+#include<vector>
+using namespace std;
+
+//HASHMAP O[NLOGN]        O[N]
+int findAllSubarraysWithGivenSum(vector < int > & nums, int k) 
+{
+    int n=nums.size();
+    int presum=0;
+    int cnt=0;
+    map<int,int>mpp;
+    mpp[0]=1;
+    for(int i=0;i<n;i++)
+    {
+        presum+=nums[i];
+        int rem=presum-k;
+        cnt+=mpp[rem];
+        mpp[presum]+=1;
+    }
+    return cnt;
+}
+
+int main()
+{
+    vector<int> arr ;
+    arr.push_back(3);
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(4);
+    int k = 6;
+    int cnt = findAllSubarraysWithGivenSum(arr, k);
+    cout << "The number of subarrays is: " << cnt << "\n";
+    return 0;
+}
