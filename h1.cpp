f6e407c906d524435d72164167ecd8acab4f1194
Abhishek Masne <104388375+abhitry@users.noreply.github.com>
2024-02-27 23:44:19 +0530

    Update h1.cpp

--- h1.cpp
+++ h1.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main()
-{
+{  
   int  nums[4]={1000000000,1000000000,1000000000,1000000000};
   long long sum=nums[0]+nums[1]+nums[2]+nums[3];
   cout<<"\nsum is =   " <<sum<<"\n\n";
