b0152c8b44c852fe4cfedf36d70a412ec2dc60c2
Abhishek Masne <abhishekmasne2015@gmail.com>
2024-03-02 16:14:28 +0530

    MERGE INTERVALS

--- h1.cpp
+++ h1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {  
-  int  nums[4]={1000000000,1000000000,1000000000,1000000000};
-  long long sum=nums[0]+nums[1]+nums[2]+nums[3];
-  cout<<"\nsum is =   " <<sum<<"\n\n";
+  
+  int x=1^2;
+  cout<<-x;
 }	
